9cb9b7287629dce96e4cf8d96f7e75ef337baa77
Charles Wight <chuck.wight@gmail.com>
2025-02-27 20:33:08 -0700

    Implemented recentQuestionKeys to reduce repeat questions

--- src/main/java/org/chemvantage/chem4ap/Score.java
+++ src/main/java/org/chemvantage/chem4ap/Score.java
@@ -4,16 +4,13 @@ import static com.googlecode.objectify.ObjectifyService.key;
 import static com.googlecode.objectify.ObjectifyService.ofy;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 
 import com.googlecode.objectify.Key;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 import com.googlecode.objectify.annotation.Parent;
-import com.googlecode.objectify.annotation.Serialize;
 
 @Entity
 public class Score {    
@@ -21,9 +18,8 @@ public class Score {
 	@Parent Key<User> owner;
 	
 	List<Long> topicIds = new ArrayList<Long>();
-	List<Key<Question>> answeredQuestionKeys = new ArrayList<Key<Question>>();
-	@Serialize
-	Map<Long,Integer> scores = new HashMap<Long,Integer>();  // scores 0-100 by topicId
+	List<Integer> scores = new ArrayList<Integer>();  // each 0-100 in order of topicId
+	List<Key<Question>> recentQuestionKeys = new ArrayList<Key<Question>>();
 	Long currentQuestionId;
 	Long nextQuestionId;
 	
@@ -33,23 +29,21 @@ public class Score {
 		id = a.id;
 		owner = key(User.class,hashedId);
 		topicIds = a.topicIds;
-		for (Long topicId : topicIds) {
-			scores.put(topicId, 0);
-		}
+		for (int i = 0; i < topicIds.size(); i++) scores.add(0);
 		currentQuestionId = getNewQuestionId();
 		nextQuestionId = getNewQuestionId();
 	}
 	
 	int getPctScore() {
 		int score = 0;
-		for (Long topicId : topicIds) score += scores.get(topicId);
+		for (int i = 0; i < topicIds.size(); i++) score += scores.get(i);
 		return score/topicIds.size();
 	}
-	
+/*	
 	void repairMe(Assignment a) {
 		for (Long tId : this.topicIds) {
 			if (!a.topicIds.contains(tId)) {  // remove any data from deleted topics
-				this.scores.remove(tId);
+				this.scores.remove(topicIds.indexOf(tId));
 			}
 		}
 		for (Long tId : a.topicIds) {  // add new data for topics that were added
@@ -60,29 +54,23 @@ public class Score {
 		this.topicIds = a.topicIds;
 		ofy().save().entity(this).now();
 	}
-
+*/
 	void update(Question q, int qScore) throws Exception {
-		StringBuffer debug = new StringBuffer("Score.update");
-		try {
 		Long topicId = q.topicId;
-		int quartile = scores.get(topicId)/25 + 1;
+		int index = topicIds.indexOf(topicId);
+		int quartile = scores.get(index)/25 + 1;
 		int n = 9;
-		int proposedScore = (120*qScore + n*scores.get(topicId))/(n+1);
+		int proposedScore = (120*qScore + n*scores.get(index))/(n+1);
 		if (proposedScore > 100) proposedScore = 100;
 		int newQuartile = proposedScore/25 + 1;
 		if (newQuartile < quartile) proposedScore = (quartile-1)*25;
-		scores.put(topicId,proposedScore);
 		
-		answeredQuestionKeys.add(key(q));
-		if (answeredQuestionKeys.size() > 5) answeredQuestionKeys.remove(0);
-	
+		// update the score item
+		scores.set(index,proposedScore);
+		
 		currentQuestionId = nextQuestionId;
-		debug.append("i");
 		nextQuestionId = getNewQuestionId();
 		ofy().save().entity(this);
-		} catch (Exception e) {
-			throw new Exception(e.getMessage() + debug.toString());
-		}
 	}
 	
 	Long getNewQuestionId() throws Exception {
@@ -92,26 +80,25 @@ public class Score {
 			Long topicId = null;
 			int range = 0;
 			for (Long tId : topicIds) {
-				range += 100 - scores.get(tId);
+				range += 100 - scores.get(topicIds.indexOf(tId));
 			}
 			Random random = new Random();
 			if (range > 0) {
 				int r = random.nextInt(range);
 				range = 0;
 				for (Long tId : topicIds) {
-					range += 100 - scores.get(tId);
+					range += 100 - scores.get(topicIds.indexOf(tId));
 					if (r < range) {
 						topicId = tId;
 						break;
 					}
 				}
-			} else {
-				topicId = topicIds.get(random.nextInt(topicIds.size()));
-			}
+			} else topicId = topicIds.get(random.nextInt(topicIds.size()));
 			
 			// Gather question keys for types based on quartile score for the topic
 			// Quartile 1 gets TF & MC, Q2 gets MC & FB, Q3 gets FB & CB, Q4 gets CB & NU
-			int quartile = scores.get(topicId)/25 + 1;
+			int score = scores.get(topicIds.indexOf(topicId));
+			int quartile = score/25 + 1;
 			if (quartile > 4) quartile = 4;
 
 			List<Key<Question>> questionKeys = new ArrayList<Key<Question>>();
@@ -130,12 +117,18 @@ public class Score {
 				questionKeys.addAll(ofy().load().type(Question.class).filter("assignmentType","Exercises").filter("topicId",topicId).filter("type","numeric").keys().list());
 			}
 
-			// Eliminate any questions recently answered
-			questionKeys.removeAll(answeredQuestionKeys);
+			// Eliminate any questions recently answered or on deck
+			questionKeys.removeAll(recentQuestionKeys);
 			
 			// Select one key at random and convert it to a Long id
-			Long questionId = questionKeys.get(random.nextInt(questionKeys.size())).getId();
-			return questionId;
+			Key<Question> k = questionKeys.get(random.nextInt(questionKeys.size()));
+			
+			// Add the key to the list of recent and trim, if necessary
+			recentQuestionKeys.add(k);
+			if (recentQuestionKeys.size() > 5) recentQuestionKeys.remove(0);
+		
+			
+			return k.getId();
 		} catch (Exception e) {
 			throw new Exception(e.getMessage() + debug.toString());
 		}
